588ac6e91745bd6d45a3149a5dd165e409ac9641
Major-shaw <atulyasingh2004@gmail.com>
2022-01-05 10:59:47 +0530

    updated code

--- ruby/bst.rb
+++ ruby/bst.rb
@@ -32,8 +32,43 @@ class BST
   def inorder(node)
     if node != nil
       self.inorder(node.left)
-      print(" ", node.data)
+      print(node.data, " ")
       self.inorder(node.right)
+    end
+  end
+
+  def preorder(node)
+    if node != nil
+      print(node.data, " ")
+      self.preorder(node.left)
+      self.preorder(node.right)
+    end
+  end
+
+  def postorder(node)
+    if node != nil
+      self.postorder(node.left)
+      self.postorder(node.right)
+      print(node.data, " ")
+    end
+  end
+
+  def levelorder(node)
+    if node == nil
+      return nil
+    end
+    queue = []
+    queue.append(node)
+
+    while queue.length() > 0
+      print(queue[0].data, " ")
+      curr = queue.shift()
+      if curr.left != nil
+        queue.append(curr.left)
+      end
+      if curr.right != nil
+        queue.append(curr.right)
+      end
     end
   end
 
@@ -142,11 +177,27 @@ class BST
     puts "" 
   end
 
-
-
   def print_inorder(node)
     puts "inorder: "
     self.inorder(node)
+    puts ""
+  end
+
+  def print_preorder(node)
+    puts "preorder: "
+    self.preorder(node)
+    puts ""
+  end
+
+  def print_postorder(node)
+    puts "postorder: "
+    self.postorder(node)
+    puts ""
+  end
+
+  def print_levelorder(node)
+    puts "levelorder: "
+    self.levelorder(node)
     puts ""
   end
 
@@ -157,8 +208,9 @@ def main()
   puts "Enter 1 to give inputs from a file, Enter to skip"
   input = gets.chomp
   if input == "1"
-    puts "getting inputs from input.txt file"
-    file = File.open("input.txt")
+    puts "Enter file name: "
+    filename = gets.chomp
+    file = File.open(filename, "r")
     file_data = file.readlines.map(&:chomp)
     tree.root = tree.insert(tree.root, file_data.shift().to_i)
     for i in file_data do
@@ -170,24 +222,31 @@ def main()
     end
   else
 
-    puts "Enter root value"
+    puts "Initialising Tree.. Enter root value: "
     root_val = gets.to_i
     tree.values.append(root_val)
     tree.root = tree.insert(tree.root, root_val)
   end
-  puts "Enter 1 to add elements, 0 to Exit"
-  puts "Enter 3 to find max element, 4 to find smallest element"
-  puts "Enter 5 to search for an element"
-  puts "Enter 6 to delete an element"
-  puts "Enter 7 to print paths from root to leaves"
+  puts "Enter 1 to add elements"
+  puts "Enter 2 to find max element, 3 to find smallest element"
+  puts "Enter 4 to search for an element"
+  puts "Enter 5 to delete an element"
+  puts "Enter 6 to print paths from root to leaves"
+  puts "Enter 7 to print inorder traversal"
+  puts "Enter 8 to print preorder traversal"
+  puts "Enter 9 to print postorder traversal"
+  puts "Enter 10 to print Levelorder"
+  puts "Enter quit to exit"
+  puts "========================================================"
 
   while true
-    input = gets.to_i
-    if input == 0
+    print ">> "
+    input = gets.chomp
+    if input == 'quit'
       File.write("tree_elements.txt", tree.values.join("\n"))
       break
     end
-    if input == 1
+    if input == '1'
       arr = []
       puts "Enter elements"
       input = gets.chomp
@@ -202,18 +261,18 @@ def main()
       end
       tree.print_inorder(tree.root)
     end
-    if input == 3
+    if input == '2'
       puts("Largest element: ", tree.find_max(tree.root))
     end
-    if input == 4
+    if input == '3'
       puts("smallest element: ", tree.find_min(tree.root))
     end 
-    if input == 5
+    if input == '4'
       puts "Enter the element you want to search"
       key = gets.to_i
       puts tree.search(tree.root, key)
     end  
-    if input == 6
+    if input == '5'
       puts "Enter the element you want to delete"
       key = gets.to_i
       if tree.search(tree.root, key) == true
@@ -224,11 +283,22 @@ def main()
       tree.print_inorder(tree.root)
     end
 
-    if input == 7
+    if input == '6'
       tree.print_paths(tree.root)
+    end
+    if input == '7'
+      tree.print_inorder(tree.root)
+    end
+    if input == '8'
+      tree.print_preorder(tree.root)
+    end
+    if input == '9'  
+      tree.print_postorder(tree.root)
+    end
+    if input == '10'
+      tree.print_levelorder(tree.root)
     end
 
   end
-  tree.print_inorder(tree.root)
-end
+ end
 main()
