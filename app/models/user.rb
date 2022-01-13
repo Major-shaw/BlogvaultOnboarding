05e955b6b92e8f6db453ca5bae7364ec8f262281
Major-shaw <atulyasingh2004@gmail.com>
2022-01-13 13:15:33 +0530

    added phase IV

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,5 +4,9 @@ class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
   has_many :comments, dependent: :destroy
-  has_many :articles, dependent: :destroy       
+  has_many :articles, dependent: :destroy
+
+  def to_s
+    "#{email}"
+  end       
 end
